a4271531f974605e4919bc87d06e99e2b046bda2
ghacupha <mailnjeru@gmail.com>
2018-06-10 15:16:03 +0300

    Created test shell for AccountIdPolicyVersion1

---
+++ src/test/java/io/github/fasset/fasset/accounts/nomenclature/policy/AccountIdPolicyVersion1Test.java
@@ -0,0 +1,29 @@
+package io.github.fasset.fasset.accounts.nomenclature.policy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountIdPolicyVersion1Test {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void currencyCode() {
+    }
+
+    @Test
+    void generalLedgerCode() {
+    }
+
+    @Test
+    void accountPlaceHolder() {
+    }
+
+    @Test
+    void accountName() {
+    }
+}
