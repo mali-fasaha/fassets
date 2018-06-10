7ad2789a45c768a90fe324a6887f6f9f78d89ea2
ghacupha <mailnjeru@gmail.com>
2018-06-10 14:46:16 +0300

    Added additional methods to the AccountIdPolicy

--- src/main/java/io/github/fasset/fasset/accounts/nomenclature/policy/AccountIdPolicy.java
+++ src/main/java/io/github/fasset/fasset/accounts/nomenclature/policy/AccountIdPolicy.java
@@ -1,5 +1,8 @@
 package io.github.fasset.fasset.accounts.nomenclature.policy;
 
+import io.github.fasset.fasset.accounts.definition.Posting;
+import io.github.fasset.fasset.accounts.definition.TransactionType;
+
 /**
  * This interface acts as building blocks to how assets are identified in front-facing systems using simple structure
  * which is data that has to be extracted from the asset attributes themselves.
@@ -18,10 +21,10 @@ package io.github.fasset.fasset.accounts.nomenclature.policy;
  * <br> Even as a developer well rehearsed with the problem space I do have or think to have attained all the light to clearly
  * perceive the problem space and its attributes, but what I do think is that the business domain can be for the yet unforeseeable
  * future, be bent to appreciate assets from the category perspective. Bigger companies would not lack the service-outlet guys,
- * the currency guy, year of manufacture and year of acquisition guys. But that would be beyond the reach of this applications.
+ * the currency guy, year of manufacture and year of acquisition guys. But that would be beyond the reach of this application.
  * Such guys should see me on the side and we can talk about how much value we need to give to my time, because such flexibility
- * would require time to implement. Or we can still try and do in this rapid development cycle, without shooting our hypothetical
- * but this is without guarantees or actually not blowing the whole damn foot in the process. One such scenario would be in
+ * would require time to implement. Or we can still try and do in this rapid development cycle, without shooting our hypothetical foot
+ * and yet this is without guarantees of actually not blowing the whole damn foot in the process. One such scenario would be in
  * a distributed environment ... oh! Here's another one, micro-services architecture. If you would think to implement mutating
  * configurations in these two you are destined to experience pain the heights of which you have not experienced before.
  *
@@ -36,4 +39,33 @@ public interface AccountIdPolicy {
      * @return Account number sequence code to follow the service outlet nomenclature
      */
     String currencyCode(String currencyCode);
+
+    /**
+     * Using the category of an asset this method returns the generic nomenclature code for the category, which in
+     * accordance to the Account nomenclature and hierarchy policy version 1.0 follows after the currency
+     * code in the account number sequence
+     *
+     * @param transactionType This is the type of fixed asset transaction and could ACQUISITION, DEPRECIATION among others
+     * @param posting The direction which we are posting. This could be DEBIT or CREDIT
+     * @param category of the asset for which we need a category code
+     * @return The category code to be added to the account number sequence after the currency code
+     */
+    String generalLedgerCode(TransactionType transactionType, Posting posting, String category);
+
+    /**
+     * Using the provided category of an asset this method returns a specific nomenclature code for the
+     * category. This is the code segment that typically follows the general ledger code in the
+     * account number sequence
+     *
+     * @return String GL Id to be used for credit transactions
+     */
+    String accountPlaceHolder(TransactionType transactionType, Posting posting, String category);
+
+    /**
+     *
+     * @param transactionType Type of transaction Enum
+     * @param category of the asset for which we seek transaction account name
+     * @return Name of the account
+     */
+    String accountName(TransactionType transactionType, Posting posting, String category);
 }
