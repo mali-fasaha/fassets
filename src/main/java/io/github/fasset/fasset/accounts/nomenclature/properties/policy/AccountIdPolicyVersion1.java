84a2d98dfa70479e2cb64e6cda6f55a060c87f49
Edwin Njeru <mailnjeru@gmail.com>
2018-06-12 18:56:51 +0300

    Implemented account number motif method

--- src/main/java/io/github/fasset/fasset/accounts/nomenclature/properties/policy/AccountIdPolicyVersion1.java
+++ src/main/java/io/github/fasset/fasset/accounts/nomenclature/properties/policy/AccountIdPolicyVersion1.java
@@ -167,4 +167,31 @@ public class AccountIdPolicyVersion1 implements AccountIdPolicy {
     public CharSequence appendant(TransactionType transaction, AccountSide accountSide) {
         return null;
     }
+
+    /**
+     * This methods combines the account segments combining them into an account number sequence which is used
+     * application wide.
+     * <br> The existence of this method makes it possible for the application to be configured to create
+     * different forms of identifications for an account number for different purposes.
+     * <br> We assume the following args are passed in the following order for this policy version
+     *         <br>i.  sol id
+     *         <br>ii. currency code
+     *         <br>iii.general ledger
+     *         <br>iv. placeholder
+     *
+     * @param accountSegments Parameters containing the various segments that form an account number
+     * @return account number sequence
+     */
+    @Override
+    public String accountNumberMotif(String... accountSegments) {
+
+        // TODO make this appendages more dynamic
+        StringBuilder sb = new StringBuilder()
+            .append(accountSegments[0])
+            .append(accountSegments[1])
+            .append(accountSegments[2])
+            .append(accountSegments[3]);
+
+        return sb.toString();
+    }
 }
