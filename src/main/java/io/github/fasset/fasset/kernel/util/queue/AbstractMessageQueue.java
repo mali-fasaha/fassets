fbc058f95edde3361361367c6cf0d2cc5e03ca5d
ghacupha <mailnjeru@gmail.com>
2018-07-08 11:34:26 +0300

    Added illustration for usage of the MQError interface in the FileUploadsQueue

--- src/main/java/io/github/fasset/fasset/kernel/util/queue/AbstractMessageQueue.java
+++ src/main/java/io/github/fasset/fasset/kernel/util/queue/AbstractMessageQueue.java
@@ -1,4 +1,91 @@
 package io.github.fasset.fasset.kernel.util.queue;
 
-public class AbstractMessageQueue {
+/**
+ * This class provides a common framework for how the completion and handle error methods should be seen.
+ * <br> The client being a MessageQueue 'producer' needs not bother with who to 'arrange' handling errors
+ * and handling completion. It only needs to provide an implementation of the {@link MessageQueue#push(QueueMessage)}
+ * method.
+ * <br> This example shows how the {@code FileUploadsQueue} 'producers' has implemented the {@link MessageQueue} interface
+ * by implementing the {@link MessageQueue#push(QueueMessage)} method. The {@link MessageQueue#push(QueueMessage, MQError)} method
+ * and the {@link MessageQueue#push(QueueMessage, MQError, MCompletion)} method become available to the {@code FileUploadQueue}
+ * simply by extending this class. Should the client desire a different approach for the lifecycle methods, nothing's wrong
+ * with it skipping this class and implementing the whole interface by itself.
+ * <br>
+ * <br> {@code @Override
+ *     public void push(QueueMessage<FileUpload> queueMessage) {
+ *
+ *         fileUploadService.recordFileUpload(queueMessage.message());
+ *     }
+ *     }
+ *
+ * <br> Note:
+ * <br> The client above simply implements the queue method push by recording the message to the {@code FileUploadService}
+ * @param <T>
+ */
+public abstract class AbstractMessageQueue<T> implements MessageQueue<T> {
+
+    /**
+     * Adds a message to the queue
+     *
+     * @param queueMessage Item to be added to the queue
+     */
+    @Override
+    public abstract void push(QueueMessage<T> queueMessage);
+
+    /**
+     * Adds a message to the queue
+     *
+     * @param queueMessage Item to be added to the queue
+     * @param mCompletion Completion lifecycle action
+     */
+    @Override
+    public void push(QueueMessage<T> queueMessage, MCompletion mCompletion) {
+
+        push(queueMessage);
+
+        // call the lifecycle method
+        mCompletion.handleCompletion();
+    }
+
+    /**
+     * Adds a message to the queue, and provides a method to allow the producer to handle error
+     *
+     * @param queueMessage Item to be added to the queue
+     * @param mqError this is a functional interface design to allow producers to have a custom way
+     *                of handling errors
+     */
+    @Override
+    public void push(QueueMessage<T> queueMessage, MQError mqError) {
+
+        try {
+
+            push(queueMessage);
+
+        } catch (MQException e) {
+
+            mqError.handleError(e);
+        }
+    }
+
+    /**
+     * Adds a message to the queue, and provides a method to allow the producer to handle error
+     *
+     * @param queueMessage Item to be added to the queue
+     * @param mqError      This is a functional interface for handling errors when they occur
+     * @param mCompletion  This is a functional interface for handling task completion tasks
+     */
+    @Override
+    public void push(QueueMessage<T> queueMessage, MQError mqError, MCompletion mCompletion) {
+
+        try {
+
+            push(queueMessage);
+
+        } catch (MQException e) {
+
+            mqError.handleError(e);
+        }
+
+        mCompletion.handleCompletion();
+    }
 }
--- src/main/java/io/github/fasset/fasset/kernel/util/queue/FileUploadsQueue.java
+++ src/main/java/io/github/fasset/fasset/kernel/util/queue/FileUploadsQueue.java
@@ -9,12 +9,14 @@ import io.github.fasset.fasset.model.files.FileUpload;
  * a file in the database.
  * <br> The lifecycle method is also called when the file is uploaded successfully but it is
  * only when the client calls the method {@link this#push(QueueMessage, MCompletion)}
+ * <br> The client could also call the method {@link this#push(QueueMessage, MQError, MCompletion)} which will handle
+ * both completion and an unexpected messageQueue runtime exception
  *
  * TODO: add validation methods here or create file validation service for the same in another
  * // Todo Add duplicate upload checks service
  * interface
  */
-public class FileUploadsQueue implements MessageQueue<FileUpload> {
+public class FileUploadsQueue extends AbstractMessageQueue<FileUpload> {
 
     private FileUploadService fileUploadService;
 
@@ -34,60 +36,4 @@ public class FileUploadsQueue implements MessageQueue<FileUpload> {
         fileUploadService.recordFileUpload(queueMessage.message());
     }
 
-    /**
-     * Adds a message to the queue
-     *
-     * @param queueMessage Item to be added to the queue
-     * @param mCompletion Completion lifecycle action
-     */
-    @Override
-    public void push(QueueMessage<FileUpload> queueMessage, MCompletion mCompletion) {
-
-        push(queueMessage);
-
-        // call the lifecycle method
-        mCompletion.handleCompletion();
-    }
-
-    /**
-     * Adds a message to the queue, and provides a method to allow the producer to handle error
-     *
-     * @param queueMessage Item to be added to the queue
-     * @param mqError this is a functional interface design to allow producers to have a custom way
-     *                of handling errors
-     */
-    @Override
-    public void push(QueueMessage<FileUpload> queueMessage, MQError mqError) {
-
-        try {
-
-            push(queueMessage);
-
-        } catch (MQException e) {
-
-            mqError.handleError(e);
-        }
-    }
-
-    /**
-     * Adds a message to the queue, and provides a method to allow the producer to handle error
-     *
-     * @param queueMessage Item to be added to the queue
-     * @param mqError      This is a functional interface for handling errors when they occur
-     * @param mCompletion  This is a functional interface for handling task completion tasks
-     */
-    @Override
-    public void push(QueueMessage<FileUpload> queueMessage, MQError mqError, MCompletion mCompletion) {
-
-        try {
-
-            push(queueMessage);
-
-        } catch (MQException e) {
-
-            mqError.handleError(e);
-        }
-
-        mCompletion.handleCompletion();
-    }
 }
